8b11a54e67459155714d589bf2316137eb1058d2
Julian König <j.koenig97@web.de>
2019-11-30 16:33:12 +0100

    use windows path seperator

--- solve.bat
+++ solve.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd .
 cd %~dp0
-if "%~3"=="" (set file_path="%~dp0input/%2.txt") else (set file_path="%~f3")
+if "%~3"=="" (set file_path="%~dp0input\%2.txt") else (set file_path="%~f3")
 popd
 %1/run %1/%2 %file_path%
