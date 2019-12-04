e5f45f884b298c7fc05a9491cba7b132d930f089
Julian König <julian.koenig@js-soft.com>
2019-12-04 17:26:45 +0100

    go: Day04 reimplement first solution

--- go/Day04.go
+++ go/Day04.go
@@ -6,30 +6,36 @@ import (
 )
 
 func main() {
-	fmt.Printf("111111 should be true: %v\n", checkCode(111111))
-	fmt.Printf("223450 should be false: %v\n", checkCode(223450))
-	fmt.Printf("123789 should be false: %v\n", checkCode(123789))
+	fmt.Printf("111111 should be true: %v\n", checkCode(111111, true))
+	fmt.Printf("223450 should be false: %v\n", checkCode(223450, true))
+	fmt.Printf("123789 should be false: %v\n", checkCode(123789, true))
 
-	fmt.Printf("112233 should be true: %v\n", checkCode(112233))
-	fmt.Printf("123444 should be false: %v\n", checkCode(123444))
-	fmt.Printf("111122 should be true: %v\n", checkCode(111122))
+	fmt.Printf("112233 should be true: %v\n", checkCode(112233, false))
+	fmt.Printf("123444 should be false: %v\n", checkCode(123444, false))
+	fmt.Printf("111122 should be true: %v\n", checkCode(111122, false))
 
 	runBetween(357253, 892942)
 }
 
 func runBetween(start int, end int) {
 	cnt := 0
-
-	//The value is within the range given in your puzzle input.
 	for i := start; i <= end; i++ {
-		if checkCode(i) {
+		if checkCode(i, true) {
 			cnt++
 		}
 	}
-	fmt.Printf("\nNumber of passwords: %d\n", cnt)
+	fmt.Printf("\nNumber of passwords 1: %d\n", cnt)
+
+	cnt = 0
+	for i := start; i <= end; i++ {
+		if checkCode(i, false) {
+			cnt++
+		}
+	}
+	fmt.Printf("\nNumber of passwords 2: %d\n", cnt)
 }
 
-func checkCode(code int) bool {
+func checkCode(code int, ignoreSeries bool) bool {
 	//It is a six-digit number.
 	strVal := strconv.Itoa(code)
 	if len(strVal) != 6 {
@@ -47,13 +53,19 @@ func checkCode(code int) bool {
 		if digit < lastVal {
 			return false
 		}
-
-		if digit == lastVal {
-			seriesLength += 1
+		if ignoreSeries {
+			if digit == lastVal {
+				same = true
+			}
 		} else {
-			same = same || seriesLength == 2
-			seriesLength = 1
+			if digit == lastVal {
+				seriesLength += 1
+			} else {
+				same = same || seriesLength == 2
+				seriesLength = 1
+			}
 		}
+
 		lastVal = digit
 	}
 
