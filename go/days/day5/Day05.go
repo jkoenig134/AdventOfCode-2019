cfc070069eb0e748fc3b1e5cbaf1f93a5029b5b7
Julian König <julian.koenig@js-soft.com>
2019-12-05 17:04:25 +0100

    go: Day05/2

--- go/days/day5/Day05.go
+++ go/days/day5/Day05.go
@@ -3,14 +3,13 @@ package day5
 import (
 	"AdventOfCode-2019/util"
 	"fmt"
-	"strconv"
 	"strings"
 )
 
 func Execute(fileContent *string) {
 	//fmt.Println(processInstructions(util.StrArrToIntArr(strings.Split("1,9,10,3,2,3,11,0,99,30,40,50", ","))))
 	//fmt.Println(processInstructions(util.StrArrToIntArr(strings.Split("1002,4,3,4,33", ","))))
-	//return
+	//fmt.Println(processInstructions(util.StrArrToIntArr(strings.Split("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", ","))))
 
 	if fileContent == nil {
 		panic("File not found.")
@@ -20,101 +19,90 @@ func Execute(fileContent *string) {
 	content := strings.Split(*fileContent, ",")
 
 	// use file content
-	fmt.Printf("First output: %d\n", processInstructions(util.StrArrToIntArr(content)))
+	processInstructions(util.StrArrToIntArr(content))
+	fmt.Println()
 }
 
 func processInstructions(intArray []int) int {
-	steps := len(intArray)
-	for i := 0; i < steps; i++ {
-		operator := intArray[i]
-		if ret := processSingle(operator, &i, intArray, "000"); ret != nil {
+	i := 0
+	for intArray[i] != 99 {
+		if ret := processSingle(&i, intArray); ret != nil {
 			return *ret
 		}
 	}
 	return -1
 }
 
-func processSingle(operator int, current *int, intArray []int, insertModes string) *int {
-	inserter := []rune(insertModes)
+func processSingle(current *int, intArray []int) *int {
+	operator := intArray[*current] % 100
+	mod1 := (intArray[*current] / 100) % 10
+	mod2 := (intArray[*current] / 1000) % 10
+	mod3 := (intArray[*current] / 10000) % 10
+
+	pointer1 := *current + 1
+	if mod1 == 0 {
+		pointer1 = intArray[*current+1]
+	}
+
+	pointer2 := *current + 2
+	if mod2 == 0 {
+		pointer2 = intArray[*current+2]
+	}
+
+	pointer3 := *current + 3
+	if mod3 == 0 {
+		pointer3 = intArray[*current+3]
+	}
 
 	switch operator {
-	case 1, 2:
-		where := *current + 3
-		if string(inserter[0]) == "0" {
-			where = intArray[where]
-		}
-
-		what1 := *current + 1
-		if string(inserter[2]) == "0" {
-			what1 = intArray[what1]
-		}
-
-		what2 := *current + 2
-		if string(inserter[1]) == "0" {
-			what2 = intArray[what2]
-		}
-
-		if operator == 1 {
-			intArray[where] = intArray[what1] + intArray[what2]
-		} else {
-			intArray[where] = intArray[what1] * intArray[what2]
-		}
-
-		*current += 3
+	case 1:
+		intArray[pointer3] = intArray[pointer1] + intArray[pointer2]
+		*current += 4
+	case 2:
+		intArray[pointer3] = intArray[pointer1] * intArray[pointer2]
+		*current += 4
 	case 3:
-		where := *current + 1
-		if string(inserter[2]) == "0" {
-			where = intArray[where]
+		var i int
+		fmt.Print("Input: ")
+		if _, err := fmt.Scanf("%d", &i); err != nil {
+			panic(err)
 		}
-		intArray[where] = 1
-		*current += 1
+
+		intArray[pointer1] = i
+		*current += 2
 	case 4:
-		where := *current + 1
-		if string(inserter[2]) == "0" {
-			where = intArray[where]
-		}
-		fmt.Print(intArray[where])
-		*current += 1
+		fmt.Print(intArray[pointer1])
+		*current += 2
 	case 5:
-		//Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the
-		// instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+		if intArray[pointer1] != 0 {
+			*current = intArray[pointer2]
+		} else {
+			*current += 3
+		}
 	case 6:
-		//Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer
-		// to the value from the second parameter. Otherwise, it does nothing.
-
+		if intArray[pointer1] == 0 {
+			*current = intArray[pointer2]
+		} else {
+			*current += 3
+		}
 	case 7:
-		//Opcode 7 is less than: if the first parameter is less than the second parameter,
-		// it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+		if intArray[pointer1] < intArray[pointer2] {
+			intArray[pointer3] = 1
+		} else {
+			intArray[pointer3] = 0
+		}
+		*current += 4
 	case 8:
-		//Opcode 8 is equals: if the first parameter is equal to the second parameter,
-		// it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-
+		if intArray[pointer1] == intArray[pointer2] {
+			intArray[pointer3] = 1
+		} else {
+			intArray[pointer3] = 0
+		}
+		*current += 4
 	case 99:
 		return &intArray[0]
 	default:
-		nextOperatorString := strconv.Itoa(operator)
-
-		if len(nextOperatorString) == 2 {
-			handleUnknownOperator(operator)
-		}
-
-		runes := []rune(nextOperatorString)
-		safeSubstring := string(runes[len(nextOperatorString)-1 : len(nextOperatorString)])
-
-		if nextOperator, _ := strconv.Atoi(safeSubstring); nextOperator > 0 && nextOperator <= 4 || nextOperator == 99 {
-			left := string(runes[:len(nextOperatorString)-2])
-			for len(left) < 3 {
-				left = "0" + left
-			}
-
-			processSingle(nextOperator, current, intArray, left)
-		} else {
-			handleUnknownOperator(nextOperator)
-		}
+		panic(fmt.Errorf("Error unknown operator: %d\n", operator))
 	}
 	return nil
-}
-
-func handleUnknownOperator(operator int) {
-	panic(fmt.Errorf("Error unknown operator: %d\n", operator))
 }
