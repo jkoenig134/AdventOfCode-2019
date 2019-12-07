abf045538b4ad5898dde5845beca85f10e42942e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-12-07 10:39:49 +0100

    Improve traversal algorithm with common ancestor instead of brute force

--- clojure/Day06.clj
+++ clojure/Day06.clj
@@ -2,30 +2,28 @@
 
 ; Returns the number of orbits the given object is contained in (i.e. direct orbit + indirect orbits)
 (defn orbit-centers [orbit-map object]
-  (count (take-while some? (drop 1 (iterate orbit-map object)))))
+  (take-while some? (drop 1 (iterate orbit-map object))))
 
 ; Returns the sum of the orbit centers for each object in the orbit map.
 (defn direct-and-indirect-orbits [orbit-map]
-  (apply + (map (partial orbit-centers orbit-map) (keys orbit-map))))
+  (apply + (map count (map (partial orbit-centers orbit-map) (keys orbit-map)))))
 
-; Returns a sequence of objects that orbit the given center
-(defn orbiting [orbit-map center]
-  (map first (filter #(= (% 1) center) orbit-map)))
+; Counts the amount of elements in coll before element appears. Returns (count coll) if it doesn't appear at all.
+(defn count-until [element coll]
+  (count (take-while (partial not= element) coll)))
 
 ; Calculates the minimum amount of traversals required in an orbit map to move from object to destination
-; (This is currently very inefficient and bad, I might still improve it)
 (defn traversals [orbit-map object destination]
-  (condp = object
-    nil Integer/MIN_VALUE
-    destination -2
-    (inc (apply max (traversals (dissoc orbit-map object) (orbit-map object) destination)
-                (for [orbit-obj (orbiting (dissoc orbit-map object) object)]
-                  (traversals (dissoc orbit-map orbit-obj) orbit-obj destination))))))
+  (let [object-centers (orbit-centers orbit-map object)
+        destination-centers (orbit-centers orbit-map destination)
+        common-ancestor (some (set destination-centers) object-centers)]
+    (+ (count-until common-ancestor object-centers)
+       (count-until common-ancestor destination-centers))))
 
 ; Parses the input format into a map of object -> orbit center
 (defn parse-orbit-map [raw]
   (apply hash-map (flatten (map (comp reverse #(.split #"\)" %)) (.split #"\n" raw)))))
 
 (def input (parse-orbit-map (slurp (first *command-line-args*))))
-(println "Total number of (in)direct orbits:" (direct-and-indirect-orbits input))
-(println "Traversals required to get from you to santa:" (traversals input "YOU" "SAN"))
+(println "Total number of (in)direct orbits:" (time (direct-and-indirect-orbits input)))
+(println "Traversals required to get from you to santa:" (time (traversals input "YOU" "SAN")))
