7a25787250a694ab2ba0ea231c452105f82fcc93
Paul2708 <paul.hoger@gmx.net>
2019-12-08 15:10:13 +0100

    Solve day08/2 in haskell

--- haskell/Day08.hs
+++ haskell/Day08.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Common
+import System.Environment
 
 type Layer = [String]
 
@@ -17,6 +18,31 @@ countInLayer layer letter = sum (map (\x -> (count x letter)) layer)
 
 find :: [Layer] -> Layer
 find layers = snd $ minimum $ [(countInLayer layer '0', layer) | layer <- layers]
+
+cover :: Layer -> Layer -> Layer
+cover layerA [] = layerA
+cover [] layerB = layerB
+cover layerA layerB = layerC
+  where
+    layerC = zipWith sCover layerA layerB
+
+printLayer :: Layer -> String
+printLayer layer = map replace (unlines layer)
+  where
+    replace a
+      | (a == '0') = '█'
+      | (a == '1') = ' '
+      | otherwise  = a      
+      
+sCover line line2 = zipWith cover2 line line2
+
+cover2 a b
+  | (a == '2')  = b
+  | otherwise = a
+
+coverAll :: [Layer] -> Layer
+coverAll layers = foldl cover [] layers
+
 {- Handle input and solve executions -}
 
 -- Solve first challenge
@@ -26,10 +52,16 @@ solve1 (Line (Just line)) = (countInLayer zeroLayer '1') * (countInLayer zeroLay
     zeroLayer = find $ parseHeight (parseWidth line 25) 6
 
 -- Solve second challenge
-solve2 :: Input -> Int
-solve2 (Lines (Just lines)) = 5
+solve2 :: Input -> String
+solve2 (Line (Just line)) = printLayer $ coverAll layers
+  where
+    layers = parseHeight (parseWidth line 25) 6
 
 -- Print challenge result
 main = do
-  solve "Transmission verification"        (Line Nothing) (solve1)
-  solve "Minimum number of orbital transfers" (Lines Nothing) (solve2)
+  solve "Transmission verification" (Line Nothing) (solve1)
+  args <- getArgs
+  line <- readLine $ head args
+  let result = solve2 (Line (Just line))
+  putStr "Decoded image:\n"
+  putStr result
