214b3fe394b40b56229b8512ec09f5cad874b35d
Paul2708 <paul.hoger@gmx.net>
2019-12-08 12:26:03 +0100

    Solve day06/2 in haskell

--- haskell/Day06.hs
+++ haskell/Day06.hs
@@ -14,6 +14,26 @@ count edges end = helper edges edges end
       | (b == end) =  1 + (helper total total a) + (helper total xs end)
       | otherwise = helper total xs end
 
+points :: (Eq t) => [Edge t] -> t -> t -> [t]
+points edges start end = helper edges edges start end
+  where
+    helper total [] start end = []
+    helper total ((Edge a b):xs) start end
+      | (a == start && b == end) = [a, b]
+      | (b == end) =  [b] ++ (helper total total start a) ++ (helper total xs start end)
+      | otherwise = helper total xs start end
+
+intersect :: [Edge String] -> [String]
+intersect edges = [a | a <- points edges "COM" "YOU", elem a (points edges "COM" "SAN")]
+
+distance :: (Eq t) => [Edge t] -> t -> t -> Int
+distance edges start end = length (points edges start end) - 1
+
+
+minPoint :: (Ord t) => [Edge t] -> [t] -> t -> t
+minPoint edges vertexes center = snd $ minimum $ map (\x ->  (distance edges x center, x)) vertexes
+
+
 {- Handle input and solve executions -}
 
 -- Solve first challenge
@@ -26,9 +46,14 @@ solve1 (Lines (Just lines)) = sum (map (count edges) targets)
 
 -- Solve second challenge
 solve2 :: Input -> Int
-solve2 (Lines (Just lines)) = 5
+solve2 (Lines (Just lines)) = (distance edges (minP "YOU") "YOU") + (distance edges (minP "SAN") "SAN") - 2
+  where
+    minP = minPoint edges (intersect edges)
+    targets = map (\(Edge a b) -> b) edges
+    edges = map parseEdge lines
+    parseEdge line = Edge ((split line ')') !! 0) ((split line ')') !! 1)
 
 -- Print challenge result
 main = do
   solve "Number of (in-)direct orbits" (Lines Nothing) (solve1)
-  solve "TODO" (Lines Nothing) (solve2)
+  solve "Minimum number of orbital transfers" (Lines Nothing) (solve2)
