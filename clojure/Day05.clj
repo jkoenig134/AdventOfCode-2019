265e09ad34f03d679c73fbd61b19c53097b537df
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-12-09 16:45:31 +0100

    Change Day05 output display

--- clojure/Day05.clj
+++ clojure/Day05.clj
@@ -5,8 +5,5 @@
 
 (def input (intcode/parse-intcodes (slurp (first *command-line-args*))))
 
-(println "Diagnostic program outputs (air conditioner):")
-(println (last (:outputs (intcode/run input 2))))
-
-(println "Diagnostic program outputs (thermal radiators):")
-(println (last (:outputs (intcode/run input 5))))
+(println "Diagnostic program outputs (air conditioner):" (last (:outputs (intcode/run input 2))))
+(println "Diagnostic program outputs (thermal radiators):" (last (:outputs (intcode/run input 5))))
