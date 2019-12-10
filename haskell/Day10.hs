506975a3b95423ee44b4e39c3dad9f5a6a3e7056
Paul2708 <paul.hoger@gmx.net>
2019-12-10 21:05:44 +0100

    Try to solve day10/2 in haskell

--- haskell/Day10.hs
+++ haskell/Day10.hs
@@ -11,14 +11,28 @@ getType :: Map -> Point -> Type
 getType [] p = error "Point not on map"
 getType ((p',t):xs) p = if (p == p') then t else getType xs p
 
+setType :: Map -> Point -> Type -> Map
+setType list p t = set list (index list 0) (p, t)
+  where 
+    index [] i = error "point not found"
+    index ((p', t'):xs) i = if p' == p then i else index xs (i + 1)    
+    
+height :: Map -> Int
+height [] = error "empty map"
+height (((a,b),t):xs) = if (a == 0) then 1 + (height xs) else 0
+
+width :: Map -> Int
+width [] = 0
+width (((a,b),t):xs) = if (b == 0) then 1 + (width xs) else 0 + (width xs)
+
 points :: Point -> Point -> [Point]
 points (a,b) (x,y)
   | (a == x) && (b < y) = [(a, b + k) | k <- [1..(y-b-1)]]
-  | (a == x) && (b > y) = points (x,y) (a,b)
+  | (a == x) && (b > y) = [(a, y + k) | k <- [1..(b-y-1)]]
   | (b == y) && (a < x) = [(a + k, b) | k <- [1..(x-a-1)]]
-  | (b == y) && (a > x) = points (x,y) (a,b)
-  | (x < a && y < b) = points (x,y) (a,b)
-  | (x < a && y > b) = points (x,y) (a,b)
+  | (b == y) && (a > x) = [(x + k, b) | k <- [1..(a-x-1)]]
+  | (x < a && b > y) && (gcd xDiff yDiff) > 1 = [(a - k*reduceX, b - k*reduceY) | k <- [1..((gcd xDiff yDiff) - 1)]]
+  | (x < a && b < y) && (gcd xDiff yDiff) > 1 = [(a - k*reduceX, b + k*reduceY) | k <- [1..((gcd xDiff yDiff) - 1)]]
   | (a < x && b < y) && (gcd xDiff yDiff) > 1 = [(a + k*reduceX, b + k*reduceY) | k <- [1..((gcd xDiff yDiff) - 1)]]
   | (a < x && b > y) && (gcd xDiff yDiff) > 1 = [(a + k*reduceX, b - k*reduceY) | k <- [1..((gcd xDiff yDiff) - 1)]]
   | otherwise = []
@@ -39,6 +53,18 @@ findStation map' = fst $ maximum [(count p, p) | p <- asteroids]
     count p = length [p' | p' <- asteroids, canSee map' p p'] - 1
     asteroids = [point | (point,t) <- map', t == Astereoid]
 
+laser :: Map -> Point -> Int -> [Point]
+laser map' (x,y) 5000 = []
+laser map' (x,y) i = case getAst (points (x,y) (boarder !! (i `mod` (length boarder)))) of Nothing -> laser map' (x,y) (i + 1)
+                                                                                           Just (a,b) -> (a,b) : (laser (setType map' (a,b) Empty) (x,y) (i + 1))
+  where
+    getAst []   = Nothing
+    getAst ((a,b):xs) = if (getType map' (a,b)) == Astereoid then Just (a,b) else getAst xs
+    
+    boarder = [(x + i, 0) | i <- [0..(w - x)]] ++ [(w, i) | i <- [1..h]] ++ [(w - i, h) | i <- [1..w]] ++ [(0, h-i) | i <- [1..h]] ++ [(i,0) | i <- [1..(x-1)]]
+    w = (width map') - 1
+    h = (height map') - 1
+  
 parseMap :: [String] -> Map
 parseMap lines = [pointAt x y | x <- [0..width], y <- [0..height]]
   where
@@ -58,10 +84,12 @@ mapIt :: Map
 mapIt = [((0,0),Empty),((0,1),Empty),((0,2),Astereoid),((0,3),Empty),((0,4),Empty),((1,0),Astereoid),((1,1),Empty),((1,2),Astereoid),((1,3),Empty),((1,4),Empty),((2,0),Empty),((2,1),Empty),((2,2),Astereoid),((2,3),Empty),((2,4),Empty),((3,0),Empty),((3,1),Empty),((3,2),Astereoid),((3,3),Empty),((3,4),Astereoid),((4,0),Astereoid),((4,1),Empty),((4,2),Astereoid),((4,3),Astereoid),((4,4),Astereoid)]
 
 -- Solve second challenge
-solve2 :: Input -> Int
-solve2 (Line (Just line)) = 5
+-- solve2 :: Input -> Int
+solve2 (Lines (Just lines)) = (laser map' (8,3) 0)
+  where
+    map' = parseMap lines
 
 -- Print challenge result
 main = do
   solve "New station coordinates"           (Lines Nothing) (solve1)
-  solve "TODO" (Line Nothing) (solve2)
+  solve "TODO" (Lines Nothing) (solve2)
