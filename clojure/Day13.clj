19d3c51db3429f7ab0d00f68c209b61f8d930432
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-12-13 19:01:55 +0100

    Remove tile type map, comment and solve day 13 fully

--- clojure/Day13.clj
+++ clojure/Day13.clj
@@ -3,39 +3,42 @@
 (ns Day13
   (:require intcode))
 
-(def tile-types
-  {0 :empty
-   1 :wall
-   2 :block
-   3 :paddle
-   4 :ball})
+; Adds or updates a tile in the provided map "tiles" given the 3-size output sequence in the format of (x y tile-type)
+(defn assoc-tile [tiles tile-output]
+  (let [x (nth tile-output 0)
+        y (nth tile-output 1)
+        type (nth tile-output 2)]
+    (assoc tiles [x y] type)))
 
-(defn add-tile [tiles tile]
-  (let [x (nth tile 0)
-        y (nth tile 1)
-        type (nth tile 2)]
-    (assoc tiles [x y] (get tile-types type type))))
+; Takes all outputs from a process state and updates the given map of tiles with them.
+(defn update-tiles [tiles state]
+  (reduce assoc-tile tiles (partition 3 (:outputs state))))
 
-(defn insert-tiles [tiles state]
-  (reduce add-tile tiles (partition 3 (:outputs state))))
-
+; Loads the game with no quarter provided and returns a map of all tiles that are created.
 (defn load-game [code]
   (let [result (intcode/run code)]
-    (insert-tiles {} result)))
+    (update-tiles {} result)))
 
+; In a map of tiles, finds the first key that is associated with the given tile type.
+(defn find-tile-position [tiles tile-type]
+  (first (filter #(= tile-type (tiles %)) (keys tiles))))
+
+; Counts the amount of tiles of the given type in a tiles map
 (defn count-tiles [tiles tile-type]
   (count (filter #{tile-type} (vals tiles))))
 
+; Plays and beats the game, returns the score at the end.
 (defn play-game [code]
   (loop [state (intcode/run (assoc code 0 2))
-         tiles (insert-tiles {} state)]
+         tiles (update-tiles {} state)]
     (if (:terminated state)
       (tiles [-1 0])
-      (let [ball-position (first (filter #(= :ball (tiles %)) (keys tiles)))
-            paddle-position (first (filter #(= :paddle (tiles %)) (keys tiles)))
-            joystick-tilt (compare (ball-position 0) (paddle-position 0))]
-        (recur (intcode/continue state joystick-tilt) (insert-tiles tiles state))))))
+      (let [ball-position (find-tile-position tiles 4)
+            paddle-position (find-tile-position tiles 3)
+            joystick-tilt (compare (ball-position 0) (paddle-position 0))
+            next-state (intcode/continue state joystick-tilt)]
+        (recur next-state (update-tiles tiles next-state))))))
 
 (def input (intcode/parse-intcodes (slurp "../input/Day13.txt")))
 (println "Number of block tiles:" (count-tiles (load-game input) :block))
-(println "Game score:" (play-game input))                   ; FIXME prints :empty
+(println "Game score:" (play-game input))
